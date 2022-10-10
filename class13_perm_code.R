7934a3ee074ee25a05a3f25f32adf35c6684273f
Yaniv Brandvain <ybrandvain@gmail.com>
2022-10-10 10:19:41 -0500

    Add files via upload

--- class13_perm_code.R
+++ class13_perm_code.R
@@ -13,8 +13,10 @@ newts <- read_csv("https://whitlockschluter3e.zoology.ubc.ca/Data/chapter13/chap
 ## PLOTS
 #################
 # plot the data [not just a box and whiskers plots]   
-ggplot(data = newts, aes(x = _____ , y = _____))  + # set up the plot
-  _________(_________________)+                     # show the data
+ggplot(data = newts, aes(x = locality , 
+                         y = whole_animal_resistance))  + # set up the plot
+  geom_jitter(width = .3, height =0, 
+              size = 3, alpha = .5)+                     # show the data
   stat_summary(fun.data = "mean_cl_boot", color = "red") # show means and error bars
 
 # plot the data as two histograms   
@@ -28,9 +30,9 @@ ggplot(data = newts, aes(___ =  ____________))  +
 #################
 
 news_summary <- newts %>%
-  _______()  + # get different data summaries for each locality
-  ______(sd_resist   = ____(____), # find standard deviations 
-         mean_resist = ____(____)) # find means
+  group_by(locality)  %>% # get different data summaries for each locality
+  dplyr::summarise(sd_resist   = sd(whole_animal_resistance), # find standard deviations 
+                   mean_resist = mean(whole_animal_resistance)) # find means
 
 
 observed_diff_mean <- newts_summary %>%
@@ -45,16 +47,32 @@ observed_diff_mean <- newts_summary %>%
 
 ### permuting_dist 
 n_perms <- 5000
-permuted_dist <- replicate(n = 1000, simplify = FALSE,
+permuted_dist <- replicate(n = n_perms, simplify = FALSE,
   newts %>%
-  mutate(perm_locality = _____(_____, replace =  ____)) %>%
+  mutate(perm_locality = sample(locality, replace =  FALSE)) %>%
   group_by(perm_locality)%>%
   summarise(mean_perm_resist = mean(whole_animal_resistance)) %>%
   summarise(mean_perm_diff = diff(mean_perm_resist)) ) %>% 
   bind_rows()
 
+
+### boot_dist 
+n_boots <- 1000
+boot_dist <- replicate(n = n_boots, simplify = FALSE,
+                       group_by(newts, locality) %>%
+                         slice_sample( prop = 1, replace = TRUE) %>%
+                          #mutate(perm_locality = sample(locality, replace =  FALSE)) %>%
+                          group_by(locality)%>%
+                          summarise(mean_boot_resist = mean(whole_animal_resistance)) %>%
+                          summarise(mean_boot_diff = diff(mean_boot_resist)) ) %>% 
+  bind_rows()
+
+ggplot(boot_dist, aes(x = abs(mean_boot_diff)))+
+  geom_histogram()+ 
+  geom_vline(xintercept = c(.426))
+#
 # p-value 
 permuted_dist %>% 
   mutate(abs_diff_means = abs(mean_perm_diff),
-         as_or_more_extreme = abs_diff_means ___ observed_diff_mean)%>%
+         as_or_more_extreme = abs_diff_means >= observed_diff_mean)%>%
   summarise(p_value = mean( as_or_more_extreme))
