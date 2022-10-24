27f065b6d88bd3384d6939451544659fa3c41f77
Yaniv Brandvain <ybrandvain@gmail.com>
2022-10-24 09:43:00 -0500

    Add files via upload

---
+++ anova_key.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+
+knee_link <- "https://whitlockschluter3e.zoology.ubc.ca/Data/chapter15/chap15e1KneesWhoSayNight.csv"
+knee_data <- read_csv(knee_link) 
+# glimpse(knee_data) # lets see what type of data we're dealing with
+# view(knee_data)    # lets look at out data
+
+knee_data <- knee_data  %>%
+  mutate(treatment = fct_relevel(treatment, "control", "knee","eyes"))
+
+################################################
+### Make  a plot! Be sure to add x and y
+################################################
+ggplot(knee_data, aes(x = treatment, y = shift, color = treatment))+
+  geom_jitter(width = .2, height = 0, size= 3, alpha = .6)+
+  stat_summary(fun.data = "mean_cl_normal", color = "black")
+
+################################################
+### run all pairwise t.tests [this is WRONG]
+################################################
+
+
+no_eyes <- dplyr::filter(knee_data, treatment  != "eyes")
+t.test(shift ~ treatment, data = no_eyes, var.equal = TRUE)
+
+
+no_knees <- dplyr::filter(knee_data, treatment  != "knee")
+t.test(shift ~ treatment, data = no_knees, var.equal = TRUE)
+
+no_control <- dplyr::filter(knee_data, treatment  != "control")
+t.test(shift ~ treatment, data = no_control, var.equal = TRUE)
+
+
+
+
+
+################################################
+### lets do an ANOVA [this is right!]
+################################################
+
+# evaluate assumptions
+
+
+# calculate all squared deviations
+knee_all2_devs <- knee_data %>% 
+  mutate(grand_mean = mean(shift)) %>% # Find the overall mean
+  group_by(treatment)              %>%
+  mutate(group_mean = mean(shift)) %>%
+  ungroup( )                       %>%
+  dplyr::mutate(total2_dev = (shift      - grand_mean)^2,
+                group2_dev = (group_mean - grand_mean)^2,
+                error2_dev = (shift      - group_mean)^2) 
+
+# calculate sums of square, mean saures, F and p-values
+knee_all2_devs %>%
+  summarise(n_group = n_distinct(treatment),
+            n_total = n(),
+            df_group = n_group - 1,
+            df_error = n_total - n_group,
+            ss2_total = sum(total2_dev),
+            ss2_group = sum(group2_dev),
+            ss2_error = sum(error2_dev),
+            mean2_group = ss2_group / df_group,
+            mean2_error = ss2_error / df_error,
+            f_val       = mean2_group /  mean2_error,
+            p_val       = pf(q = f_val, 
+                             df1 = df_group, df2 = df_error, 
+                             lower.tail = FALSE)
+            ) 
+
+
+# or with aov()
+knee_anova <- aov(shift ~ treatment, data = knee_data)
+knee_anova
+summary(knee_anova)    # Lets check out the ANOVA table
+TukeyHSD(knee_anova)   # Lets do a posthoc test to see which means differ
+
+# compare to all t-tests above... how different?
+# conclude
